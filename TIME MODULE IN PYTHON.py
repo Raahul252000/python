07108445b4a7e51f2a15d731ca448f0d099c9512
Raahul252000 <101476794+Raahul252000@users.noreply.github.com>
2022-05-11 16:53:03 +0530

    Add files via upload

---
+++ TIME MODULE IN PYTHON.py
@@ -0,0 +1,24 @@
+# import time
+# initial1 = time.time()
+# k = 0
+# while k <=50:
+#     print("This is rahul")
+#     k +=1
+# print("while loop ran in",time.time()-initial1,"seconds")
+#
+# initial2 = time.time()
+# for i in range(50):
+#     print("This is sam")
+# print("for loop ran in",time.time()-initial1,"seconds")
+import time
+import datetime
+
+print(localtime)
+print(time.asctime())
+print(datetime.datetime.now())
+
+for i in range (10):
+    print("loading...")
+    time.sleep(2)
+
+
---
+++ USING WITH BLOCK TO OPEN FILE.py
@@ -0,0 +1,4 @@
+with open("temp1.txt") as f:
+    print(f.read())
+f = open("temp1.txt")
+print(f.read())
